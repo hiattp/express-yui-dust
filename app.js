bbbffe9f5aebf9e4d4c6368b89aa1dfacf651bd7
Paul Hiatt <hiattp@gmail.com>
2012-04-16 17:40:33 -0400

    cleanup

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'ejs');
+	app.set('view options', {layout: false});
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(require('stylus').middleware({ src: __dirname + '/public' }));
